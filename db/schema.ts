1a1bcaf08cac9cd17a74f5f318e267576efa53bd
Henrique Costa <henrique@nois.agency>
2025-02-03 21:30:09 -0300

    Publish 1 from localhost into origin/main

--- db/schema.ts
+++ db/schema.ts
@@ -3,12 +3,17 @@
 * The code snippet below is an example.
 */
 
-/**
-* import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const profiles = sqliteTable("profiles", {
+export const leads = sqliteTable("leads", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   name: text("name"),
-  email: text("email"),
+  email: text("email"), 
+  phone: text("phone"), 
 });
-*/
+
+export const products = sqliteTable("prdocuts", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  name: text("name"), 
+});
+
